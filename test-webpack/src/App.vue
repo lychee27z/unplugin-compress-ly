7eaa273f88c205ba700219146f13952df2da0f5d
张子健 <zhangzijian955@sf-express.com>
2023-11-22 09:44:49 +0800

    feat: 插件webpack项目调试

--- test-webpack/src/App.vue
+++ test-webpack/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>webpack5-vue3-ts</h2>
+  <div>
+    <img
+      style="width: 1200px"
+      src="./assets/image/wallhaven-x61xdo.jpg"
+      alt=""
+    />
+    <!-- <div class="a"></div> -->
+    <div class="b"></div>
+    <!-- <img src="./assets/shuimo.svg" alt="" /> -->
+    <img src="/img/a.png" alt="" />
+    <img src="/img/b.png" alt="" />
+    <img src="/img/c.png" alt="" />
+    <img src="/img/d.png" alt="" />
+    <!-- <router-link to="/home">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+    <RouterView /> -->
+    <!-- <img src="./assets/vuetify.svg" alt="" /> -->
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup lang="ts">
+import "./app.less";
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
